30edac777592ea1b0a9bbb52811c8cc3d92312b0
aronei44 <meliodasmeliodas224@gmail.com>
2023-09-17 07:32:08 +0700

    test (note test): Warning: Test still has a runtime error from pytest

---
+++ Test/__init__.py
@@ -0,0 +1,2 @@
+from .main import Main
+from .note import Note
--- test_main.py
+++ test_main.py
@@ -1,12 +1,31 @@
 from fastapi.testclient import TestClient
 from main import app
 
-from Test.main import Main
+from Test import Main, Note
 
 
 
 client = TestClient(app)
-
+graph_url = "/graphql"
 
 def test_read_main():
    Main.test_read_main(client)
+
+def test_get_all_note():
+   Note.test_get_all_note(client, graph_url)
+
+def test_get_note_by_id():
+   Note.test_get_note_by_id(client, graph_url)
+
+def test_create_note():
+   Note.test_create_note(client, graph_url)
+
+def test_update_note():
+   Note.test_update_note(client, graph_url)
+
+def test_delete_note():
+    Note.test_delete_note_by_id(client, graph_url)
+
+"""
+WARNING: test still error because run time error from pytest
+"""
